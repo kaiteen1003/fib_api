5eea6305664cfdb814bb757884e6802a8d444137
kaiteen1003 <piasonica1003@gmail.com>
2025-05-15 00:07:40 +0900

    CORSの追加

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from my_package.fibonacci import fibonacci
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/fib", methods=["GET"])
